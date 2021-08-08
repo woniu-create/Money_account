93ca041ba66ded149a7f4e828f1be7fc4a4e348d
luyandan <2103029766@qq.com>
2021-08-08 21:50:12 +0800

    添加icon

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div id="app">
     <div class="content">
-     <router-view/>
+      <router-view />
     </div>
     <div id="nav">
-      <router-link to="/">Money</router-link> |
-      <router-link to="/detail">Detail</router-link> |
-      <router-link to="/charts">Charts</router-link>
+      <router-link to="/detail" class="item">
+        <svg class="icon">
+          <use xlink:href="#icon-billingdetails"></use>
+        </svg>
+        明细
+      </router-link>
+      <router-link to="/" class="item">
+        <svg class="icon">
+          <use xlink:href="#icon-xiaofeizonge"></use>
+        </svg>
+        记账
+      </router-link>
+      <router-link to="/charts" class="item">
+        <svg class="icon">
+          <use xlink:href="#icon-areachart"></use>
+        </svg>
+        图标
+      </router-link>
     </div>
   </div>
 </template>
 
 <style>
 #app {
-display: flex;
-height: 100vh;
-flex-direction: column;
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
 }
 .content {
   flex-grow: 1;
@@ -23,15 +38,30 @@ flex-direction: column;
   border: 1px solid green;
 }
 #nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: row;
+  font-size: 12px;
+  background-color: #272a3b;
 }
-
+#nav .item {
+  padding: 2px 0;
+  width: 33.33333%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+#nav .item .icon {
+      width: 32px;
+      height: 32px;
+    }
 #nav a {
   /* font-weight: bold;
   color: #2c3e50; */
+  color: #c8c8c8;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #e1800b;
 }
 </style>
